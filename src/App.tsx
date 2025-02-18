9c694408808f9949ba68be798e7b86b6515cbbda
Daniel Salas <danielsalas094@gmail.com>
2025-02-17 19:16:03 -0500

    Barra de navegacion

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-import Routes from "./Routes";
-import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
-function App() {
+const Inicio = () => <h1>Inicio</h1>;
+const Servicio = () => <h1>Servicio</h1>;
+const Nosotros = () => <h1>Nosotros</h1>;
+const Proyectos = () => <h1>Proyectos</h1>;
+const Contacto = () => <h1>Contacto</h1>;
+
+const App: React.FC = () => {
   return (
-    <div>
-      <BrowserRouter basename="">
-        <Routes />
-      </BrowserRouter>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/servicio" element={<Servicio />} />
+        <Route path="/nosotros" element={<Nosotros />} />
+        <Route path="/proyectos" element={<Proyectos />} />
+        <Route path="/contacto" element={<Contacto />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,48 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
-const Navbar = () => {    
+
+// Estilos css
+const NavbarContainer = styled.nav`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+  padding: 15px 30px;
+  background: #F1F1F1;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.2);
+  position: fixed;
+  width: 100%;
+  height: 5%;
+  top: 0;
+  left: 0;
+`;
+
+const NavLink = styled(Link)`
+  text-decoration: none;
+  color: black;
+  font-size: 18px;
+  font-weight: bold;
+  transition: color 0.3s ease-in-out, transform 0.2s ease;
+
+  &:hover {
+    color: #ff9800;
+    transform: scale(1.1);
+  }
+`;
+
+// Navbar
+const Navbar: React.FC = () => {
   return (
-    <nav>
+    <NavbarContainer>     
+      <NavLink to="/inicio">Inicio</NavLink>
+      <NavLink to="/servicio">Servicio</NavLink>
+      <NavLink to="/nosotros">Nosotros</NavLink>
+      <NavLink to="/contacto">Contacto</NavLink>
+    </NavbarContainer>
+  );
+};
 
-    </nav>
-  )
-}
 
-export default Navbar
+export default Navbar;
